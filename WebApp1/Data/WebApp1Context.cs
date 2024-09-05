4643ca8239dadcb6d655f8460618eeae599e8857
Gabriel Fallavena <fallavena.1903@gmail.com>
2024-09-05 16:39:14 -0300

    Departments criado e vinculado ao MySql

---
+++ WebApp1/Data/WebApp1Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp1.Models
+{
+    public class WebApp1Context : DbContext
+    {
+        public WebApp1Context (DbContextOptions<WebApp1Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;port=3306;database=bancoteste2;user=root;password=47361903;",
+                new MySqlServerVersion(new Version(8, 0, 21)));
+            }
+        }
+
+        public DbSet<WebApp1.Models.Department> Department { get; set; } = default!;
+    }
+}
